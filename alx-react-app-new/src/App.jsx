d2f6d3a8bd508b70a5422efae2d178df4c85b9d7
dev-meron <merijourney97@gmail.com>
2026-01-21 12:41:47 -0600

    Add WelcomeMessage and Counter components to App

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,27 +1,26 @@
 // src/App.jsx
-
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
+import Counter from './components/Counter';
 
 function App() {
     return (
         <div>
-            {/* Task 2 components */}
+            <WelcomeMessage />
             <Header />
             <MainContent />
             <Footer />
-
-            {/* Task 3: UserProfile with props */}
             <UserProfile 
                 name="Alice" 
                 age="25" 
                 bio="Loves hiking and photography" 
             />
+            <Counter />
         </div>
     );
 }
 
 export default App;
-
