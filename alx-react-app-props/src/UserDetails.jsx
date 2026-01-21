a4464ea1720b0607055e7777e38c4d04e042edb4
dev-meron <merijourney97@gmail.com>
2026-01-21 13:23:59 -0600

    Update UserDetails.jsx

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,10 +1,4 @@
-// src/UserDetails.jsx
-import { useContext } from 'react';
-import UserContext from './UserContext';
-
-function UserDetails() {
-  const userData = useContext(UserContext);
-
+function UserDetails({ userData }) {
   return (
     <div>
       <p>Name: {userData.name}</p>
