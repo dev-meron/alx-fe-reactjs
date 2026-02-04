198cbb6db6ea9009f3b774ea5372378f184f9c56
dev-meron <merijourney97@gmail.com>
2026-02-04 17:22:24 -0600

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,15 +1,24 @@
 import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
-  // initial state
   recipes: [],
 
-  // action: add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
-  // action: replace the whole recipe list
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
   setRecipes: (recipes) => set({ recipes }),
 }));
