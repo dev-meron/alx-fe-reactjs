0aecd8debffad8510531efda5dbec793018ef09f
dev-meron <merijourney97@gmail.com>
2026-02-04 17:39:29 -0600

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,12 @@
-import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div style={{ padding: '20px' }}>
       <h1>Recipe Sharing App</h1>
+      <SearchBar />
       <AddRecipeForm />
       <RecipeList />
     </div>
