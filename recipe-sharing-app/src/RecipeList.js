4b9c98a2b748ad9191023e13a4ebeb6505bec044
dev-meron <merijourney97@gmail.com>
2026-02-04 17:25:32 -0600

    Update RecipeList.js

---
+++ recipe-sharing-app/src/RecipeList.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from '../recipestore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
