1f8894186834d4fd144aff8bd009019b7479e6d4
dev-meron <merijourney97@gmail.com>
2026-02-04 18:16:04 -0600

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import SearchBar from './components/SearchBar';
+import Search from './components/Search';
 
 function App() {
   return (
     <div style={{ padding: '20px' }}>
       <h1>GitHub User Search</h1>
-      <SearchBar />
-      {/* Results will be displayed here later */}
+      <Search />
     </div>
   );
 }
